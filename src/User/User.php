10bd2fd3c3f38212fa35896d92fe83faadf17113
CurtisBarogla <curtis_barogla@outlook.fr>
2017-11-06 22:43:42 +0100

    Basic user implementation

---
+++ src/User/User.php
@@ -0,0 +1,189 @@
+<?php
+//StrictType
+declare(strict_types = 1);
+
+/*
+ * Zoe
+ * Security component
+ *
+ * Author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+
+namespace Zoe\Component\Security\User;
+
+use Zoe\Component\Security\Exception\InvalidUserAttributeException;
+
+/**
+ * Basic user implementation
+ * 
+ * @author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+class User implements UserInterface
+{
+    
+    /**
+     * User identifier
+     * 
+     * @var string
+     */
+    private $name;
+    
+    /**
+     * User password
+     * 
+     * @var string
+     */
+    private $password;
+    
+    /**
+     * User extra attributes
+     * 
+     * @var array|null
+     */
+    private $attributes = null;
+    
+    /**
+     * Roles 
+     * 
+     * @var array
+     */
+    private $roles = [];
+    
+    /**
+     * Is root
+     * 
+     * @var bool
+     */
+    private $isRoot = false;
+    
+    /**
+     * Initialize a basic user
+     * 
+     * @param string $name
+     *   User name
+     * @param string $password
+     *   User password
+     * @param bool $isRoot
+     *   If the user is root
+     */
+    public function __construct(string $name, string $password, bool $isRoot = false)
+    {
+        $this->name = $name;
+        $this->password = $password;
+        $this->isRoot = $isRoot;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Zoe\Component\Security\User\UserInterface::getName()
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Zoe\Component\Security\User\UserInterface::getPassword()
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set a new role for the user
+     * 
+     * @param string $role
+     *   Role to add
+     * 
+     * @return self
+     *   self
+     */
+    public function addRole(string $role): self
+    {
+        $this->roles[$role] = $role;
+        
+        return $this;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Zoe\Component\Security\User\UserInterface::getRoles()
+     */
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Zoe\Component\Security\User\UserInterface::hasRole()
+     */
+    public function hasRole(string $role): bool
+    {
+        return isset($this->roles[$role]);
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Zoe\Component\Security\User\UserInterface::isRoot()
+     */
+    public function isRoot(): bool
+    {
+        return $this->isRoot;
+    }
+    
+    /**
+     * Add an extra attribute to the user
+     * 
+     * @param string $name
+     *   Attribute name
+     * @param mixed $value
+     *   Attribute value
+     * 
+     * @return self
+     *   self 
+     */
+    public function addAttribute(string $name, $value): self
+    {
+        $this->attributes[$name] = $value;
+        
+        return $this;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Zoe\Component\Security\User\UserInterface::getAttributes()
+     */
+    public function getAttributes(): ?array
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Zoe\Component\Security\User\UserInterface::getAttribute()
+     */
+    public function getAttribute(string $name)
+    {
+        if(!isset($this->attributes[$name]))
+            throw new InvalidUserAttributeException(\sprintf("This attribute '%s' for user '%s' is not setted",
+                $name,
+                $this->name));
+            
+        return $this->attributes[$name];
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Zoe\Component\Security\User\UserInterface::hasAttribute()
+     */
+    public function hasAttribute(string $name): bool
+    {
+        return isset($this->attributes[$name]);
+    }
+
+}
---
+++ tests/User/UserTest.php
@@ -0,0 +1,178 @@
+<?php
+//StrictType
+declare(strict_types = 1);
+
+/*
+ * Zoe
+ * Security component
+ *
+ * Author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+
+namespace ZoeTest\Component\Security\User;
+
+use PHPUnit\Framework\TestCase;
+use Zoe\Component\Security\Exception\InvalidUserAttributeException;
+use Zoe\Component\Security\User\User;
+use Zoe\Component\Security\User\UserInterface;
+
+/**
+ * User testcase
+ * 
+ * @see \Zoe\Component\Security\User\User
+ * 
+ * @author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+class UserTest extends TestCase
+{
+    
+    /**
+     * @see \Zoe\Component\Security\User\User
+     */
+    public function testInterface(): void
+    {
+        $user = new User("foo", "bar");
+        
+        $this->assertInstanceOf(UserInterface::class, $user);
+    }
+    
+    /**
+     * @see \Zoe\Component\Security\User\User::getName()
+     */
+    public function testGetName(): void
+    {
+        $user = new User("foo", "bar");
+        
+        $this->assertSame("foo", $user->getName());
+    }
+    
+    /**
+     * @see \Zoe\Component\Security\User\User::getPassword()
+     */
+    public function testGetPassword(): void
+    {
+        $user = new User("foo", "bar");
+        
+        $this->assertSame("bar", $user->getPassword());
+    }
+    
+    /**
+     * @see \Zoe\Component\Security\User\User::addRole()
+     */
+    public function testAddRole(): void
+    {
+        $user = new User("foo", "bar");
+        
+        $this->assertInstanceOf(UserInterface::class, $user->addRole("foo"));
+    }
+    
+    /**
+     * @see \Zoe\Component\Security\User\User::addRole()
+     */
+    public function testGetRoles(): void
+    {
+        $user = new User("foo", "bar");
+        
+        $user->addRole("foo")->addRole("bar");
+        $expected = ["foo" => "foo", "bar" => "bar"];
+        
+        $this->assertSame($expected, $user->getRoles());
+    }
+    
+    /**
+     * @see \Zoe\Component\Security\User\User::hasRole()
+     */
+    public function testHasRole(): void
+    {
+        $user = new User("foo", "bar");
+        
+        $user->addRole("foo");
+        $this->assertTrue($user->hasRole("foo"));
+        $this->assertFalse($user->hasRole("bar"));
+    }
+    
+    /**
+     * @see \Zoe\Component\Security\User\User::isRoot()
+     */
+    public function testIsRoot(): void
+    {
+        $user = new User("foo", "bar");
+        
+        $this->assertFalse($user->isRoot());
+        
+        $user = new User("foo", "bar", true);
+        
+        $this->assertTrue($user->isRoot());
+    }
+    
+    /**
+     * @see \Zoe\Component\Security\User\User::addAttribute()
+     */
+    public function testAddAttribute(): void
+    {
+        $user = new User("foo", "bar");
+        
+        $this->assertInstanceOf(UserInterface::class, $user->addAttribute("foo", "bar")->addAttribute("bar", "foo"));
+    }
+    
+    /**
+     * @see \Zoe\Component\Security\User\User::getAttributes()
+     */
+    public function testGetAttributes(): void
+    {
+        $user = new User("foo", "bar");
+        
+        $expected = ["foo" => "bar", "bar" => "foo"];
+        $user->addAttribute("foo", "bar")->addAttribute("bar", "foo");
+        
+        $this->assertSame($expected, $user->getAttributes());
+        
+        $user = new User("foo", "bar");
+        
+        $this->assertNull($user->getAttributes());
+    }
+    
+    /**
+     * @see \Zoe\Component\Security\User\User::getAttribute()
+     */
+    public function testGetAttribute(): void
+    {
+        $user = new User("foo", "bar");
+        
+        $user->addAttribute("foo", "bar");
+        
+        $this->assertSame("bar", $user->getAttribute("foo"));
+    }
+    
+    /**
+     * @see \Zoe\Component\Security\User\User::hasAttribute()
+     */
+    public function testHasAttribute(): void
+    {
+        $user = new User("foo", "bar");
+        
+        $this->assertFalse($user->hasAttribute("foo"));
+        
+        $user->addAttribute("foo", "bar");
+        
+        $this->assertTrue($user->hasAttribute("foo"));
+    }
+    
+                    /**_____EXCEPTIONS_____**/
+    
+    /**
+     * @see \Zoe\Component\Security\User\User::getAttribute()
+     */
+    public function testExceptionWhenTryingToGetAnInvalidAttribute(): void
+    {
+        $this->expectException(InvalidUserAttributeException::class);
+        $this->expectExceptionMessage("This attribute 'foo' for user 'user' is not setted");
+        
+        $user = new User("user", "bar");
+        
+        $user->getAttribute("foo");
+    }
+    
+}
