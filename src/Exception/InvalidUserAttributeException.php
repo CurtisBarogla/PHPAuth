0114eeb7db201e7e95993fd08063a4c9ed4eb0cc
CurtisBarogla <curtis_barogla@outlook.fr>
2017-11-15 23:37:38 +0100

    Rework on exception

--- src/Exception/InvalidUserAttributeException.php
+++ src/Exception/InvalidUserAttributeException.php
@@ -12,13 +12,30 @@ declare(strict_types = 1);
 
 namespace Zoe\Component\Security\Exception;
 
+use Zoe\Component\Security\User\Contracts\UserInterface;
+
 /**
  * InvalidUserAttributeException Security component
  * 
  * @author CurtisBarogla <curtis_barogla@outlook.fr>
  *
  */
-class InvalidUserAttributeException extends \InvalidArgumentException
+class InvalidUserAttributeException extends \Exception
 {
-    //    
+    
+    /**
+     * Initialize exception
+     * 
+     * @param UserInterface $user
+     *   User which the attribute is invalid
+     * @param string $invalidAttribute
+     *   Invalid attribute name
+     */
+    public function __construct(UserInterface $user, string $invalidAttribute)
+    {
+        parent::__construct(\sprintf("This attribute '%s' for the user '%s' is not setted",
+            $invalidAttribute,
+            $user->getName()));
+    }
+    
 }
