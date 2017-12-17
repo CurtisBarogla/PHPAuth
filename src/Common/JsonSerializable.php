1c20588a257eed7d0588e1507149bad8a80822b1
CurtisBarogla <curtis_barogla@outlook.fr>
2017-12-17 11:18:48 +0100

    Common interface for all objects jsonable

---
+++ src/Common/JsonSerializable.php
@@ -0,0 +1,36 @@
+<?php
+//StrictType
+declare(strict_types = 1);
+
+/*
+ * Zoe
+ * Security component
+ *
+ * Author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+
+namespace Zoe\Component\Security\Common;
+
+/**
+ * Extends basic interface to set a "factory like" object creation from a his json representation
+ *  
+ * @author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+interface JsonSerializable extends \JsonSerializable
+{
+    
+    /**
+     * Restore an object from his json representation.
+     * This representation can be either a string or an array
+     * 
+     * @param string|array $json
+     *   Json array or string object representation
+     *   
+     * @return mixed
+     *   Restored object
+     */
+    public static function restoreFromJson($json);
+    
+}
