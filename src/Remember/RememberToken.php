f65fef76bddc735a72f22fe4997ff7025d42c2bf
CurtisBarogla <curtis_barogla@outlook.fr>
2022-10-12 10:05:38 +0200

    Implementation of the remember token

---
+++ src/Remember/RememberToken.php
@@ -0,0 +1,26 @@
+<?php
+//StrictType
+declare(strict_types = 1);
+
+/*
+ * Ness
+ * Authentication component
+ *
+ * Author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+ 
+namespace Ness\Component\Authentication\Remember;
+
+/**
+ * Remember token
+ * 
+ * @author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+class RememberToken
+{
+    
+    private $id;
+    
+}
